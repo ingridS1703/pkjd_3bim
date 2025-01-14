26030130a87e879378956e1039d1cf6b74869cff
ingridS1703 <i.sayonara@escolar.ifrn.edu.br>
2025-01-14 08:17:58 -0300

    14.01.25
    colisão e script do player

---
+++ algoritimos_2semstre/Assets/Scenes/Sprits/player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int numero = 10;
+    void Start()
+    {   
+        Debug.Log(message: "Teste..."+numero);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
